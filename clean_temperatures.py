54f4f10d54c576cec96b03c626b9e384cad2af3b
erpeg <mac.kielek@gmail.com>
2020-04-05 19:10:03 +0200

    finished files

--- clean_temperatures.py
+++ clean_temperatures.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import argparse
-import pandas
+import csv
 
 def file_to_nested_list(file_name):
     with open(file_name) as file:
@@ -9,51 +9,68 @@ def file_to_nested_list(file_name):
         file = [line.replace('\"', '').split(',') for line in file]
     return file
 
+# checking if any value of list is equal to 'NA'
+def if_na(list):
+    for element in list:
+        if element == 'NA':
+            return False
+    return True
+
+# fahr to celsius converter
+def fahr_to_cel(fahr):
+    try:
+        if fahr == 'AverageTemperatureFahr':
+            fahr = 'AverageTemperatureCelsius'
+            return fahr
+        elif fahr == 'AverageTemperatureUncertaintyFahr':
+            fahr = 'AverageTemperatureUncertaintyCelsius'
+            return fahr
+        else:
+            celsius = round((float(fahr) - 32) * 5/9, 4)
+            return celsius
+    except:
+        print('Improper data given')
 
 def main():
     # creating a parser
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file_name', type=str, help='Pass name of file to clean')
+    parser.add_argument('-i', '--input_name', type=str, help='Pass name of file to clean')
+    parser.add_argument('-o', '--out_name', type=str, help='Pass name of output file')
     args = parser.parse_args()
 
-    list_of_temps = file_to_nested_list(args.file_name)
-
+    list_of_temps = file_to_nested_list(args.input_name)
     nr_categories = len(list_of_temps[0])
 
     # removing incomplete lines
     list_of_temps = [line for line in list_of_temps if len(line) == nr_categories]
 
-    new_list = []
+    # removing lines with 'NA' values
+    new_list = [line for line in list_of_temps if if_na(line)]
 
-    # removing lines with lacking temperatures
-    # 4 - Avg temp
-    # 5 - Avg Unc Temp
-    # 6 - City
-    # 8 - Country
-    for line in list_of_temps:
-        if line[4] == 'NA':
-            break
-        elif line[5] == 'NA':
-            break
-        elif line[6] == 'NA':
-            break
-        elif line[8] == 'NA':
-            break
-        else:
-            new_list.append(line)
+    col_names = new_list[0]
 
+    for line in new_list:
+        # removing day column
+        line.pop(2)
+        # converting fahrenheit to celsius
+        line[3] = fahr_to_cel(line[3])
+        line[4] = fahr_to_cel(line[4])
+        for i in range(0, 3):    # converting first 3 cols to int
+            if line[i] in col_names:
+                break
+            else:
+                line[i] = int(line[i])
 
-    # list_of_temps = [line for line in list_of_temps if line[4] != 'NA' and line[5] != 'NA'] # poprawić conditions
-    # list_of_temps = [line for line in list_of_temps if line[6] != 'NA' and line[8] != 'NA'] # poprawić conditions
-
-
-
-
-
-    for line in new_list[0:50]:
-        print(line)
-    print(len(list_of_temps[-1]))
-
+    # saving to file
+    if args.out_name:
+        with open(args.out_name, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(new_list)
+        print('File saved as: ' + str(args.out_name))
+    # showing first 10 rows
+    else:
+        for line in new_list[0:10]:
+            print(line)
 
 if __name__ == '__main__':
     main()
